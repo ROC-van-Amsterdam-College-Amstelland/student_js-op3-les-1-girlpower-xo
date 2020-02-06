30080080651ba3fde2d5b80687aeb905fe90dffa
Iris Petersen <iris.petersen@student.rocva.nl>
2020-02-06 10:39:57 +0100

    opdracht 1 done

--- opdracht01/js/script.js
+++ opdracht01/js/script.js
@@ -2,9 +2,12 @@ var stop = "rood";
 var maybe = "oranje";
 var go = "groen";
 
-window.onload = function(){
+window.onload = function () {
     //JOUW CODE HIER
+    document.getElementById("light-top").innerHTML = go;
+    document.getElementById("light-middle").innerHTML = maybe;
+    document.getElementById("light-bottem").innerHTML = stop;
 
-    
+
 }
 
